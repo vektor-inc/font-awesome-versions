9fe340b759cffdcc24a189f04067fd857bf586dc
drill-lancer <rickaddison7634@gmail.com>
2023-03-20 10:49:40 +0900

    update: font-awesome 6.3.0

--- src/VkFontAwesomeVersions.php
+++ src/VkFontAwesomeVersions.php
@@ -162,7 +162,7 @@ class VkFontAwesomeVersions {
 		$versions = array(
 			'6_SVG_JS'       => array(
 				'label'   => '6 SVG with JS ( ' . __( 'Not recommended', 'font-awesome-versions' ) . ' )',
-				'version' => '6.1.0',
+				'version' => '6.3.0',
 				'type'    => 'svg-with-js',
 				/* [ Notice ] use editor css*/
 				'url_css' => $font_awesome_directory_uri . 'versions/6/css/all.min.css',
@@ -170,7 +170,7 @@ class VkFontAwesomeVersions {
 			),
 			'6_WebFonts_CSS' => array(
 				'label'   => '6 Web Fonts with CSS',
-				'version' => '6.1.0',
+				'version' => '6.3.0',
 				'type'    => 'web-fonts-with-css',
 				'url_css' => $font_awesome_directory_uri . 'versions/6/css/all.min.css',
 				'url_js'  => '',
