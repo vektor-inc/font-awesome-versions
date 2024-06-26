3f86a2e0f78ac5302f9123d57a1be121a3affe6b
drill-lancer <rickaddison7634@gmail.com>
2024-06-26 09:50:57 +0900

    fix: gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const replace = require('gulp-replace');
  * Copy Font Awesome
  */
 gulp.task('copy_fa', function(done) {
-    gulp.src('./node_modules/@fortawesome/fontawesome-free/css/all.min.css')
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/css/all.min.css', {encoding: false})
         .pipe(replace('*/', '*/\n\n/* .editor-styles-wrapper がないと 5.9 のブロックパターン挿入プレビューやタブレットで読み込まれない(2022.2.1現在)ので応急対応 */\n.editor-styles-wrapper{}'))
         .pipe(gulp.dest('./src/versions/6/css/'))
-    gulp.src('./node_modules/@fortawesome/fontawesome-free/js/all.min.js')
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/js/all.min.js', {encoding: false})
         .pipe(gulp.dest('./src/versions/6/js/'))
-    gulp.src('./node_modules/@fortawesome/fontawesome-free/sprites/**')
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/sprites/**', {encoding: false})
         .pipe(gulp.dest('./src/versions/6/sprites/'))
-    gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**')
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**', {encoding: false})
         .pipe(gulp.dest('./src/versions/6/webfonts/'))
     done();
 });
