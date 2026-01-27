fdf60ed983d3c02f5d9eafe73fc65c6d0f0ca19a
drill-lancer <rickaddison7634@gmail.com>
2026-01-27 13:41:46 +0900

    fix: css handle

--- src/VkFontAwesomeVersions.php
+++ src/VkFontAwesomeVersions.php
@@ -404,13 +404,16 @@ class VkFontAwesomeVersions {
 	public static function load_gutenberg_font_awesome() {
 		$current_info = self::current_info();
 		$options = self::get_option_fa();
-		wp_enqueue_style( 'vk-font-awesome', $current_info['url_css'], array(), $current_info['version'] );
+		if ( ! is_admin() ) {
+			return;
+		}
+		wp_enqueue_style( 'gutenberg-font-awesome', $current_info['url_css'], array(), $current_info['version'] );
 		if ( ! empty( $options['compatibility']['v4'] ) ) {
-			wp_enqueue_style( 'vk-font-awesome-v4-shims', $current_info['url_v4-shims_css'], array( 'vk-font-awesome' ), $current_info['version'] );
-			wp_enqueue_style( 'vk-font-awesome-v4-font-face', $current_info['url_v4-font-face_css'], array( 'vk-font-awesome' ), $current_info['version'] );
+			wp_enqueue_style( 'gutenberg-font-awesome-v4-shims', $current_info['url_v4-shims_css'], array( 'gutenberg-font-awesome' ), $current_info['version'] );
+			wp_enqueue_style( 'gutenberg-font-awesome-v4-font-face', $current_info['url_v4-font-face_css'], array( 'gutenberg-font-awesome' ), $current_info['version'] );
 		}
 		if ( ! empty( $options['compatibility']['v5'] ) ) {
-			wp_enqueue_style( 'vk-font-awesome-v5-font-face', $current_info['url_v5-font-face_css'], array( 'vk-font-awesome' ), $current_info['version'] );
+			wp_enqueue_style( 'gutenberg-font-awesome-v5-font-face', $current_info['url_v5-font-face_css'], array( 'gutenberg-font-awesome' ), $current_info['version'] );
 		}
 	}
 
